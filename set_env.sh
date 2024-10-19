f647c12e9386bcb096b5da977009cda77c566f48
ToanFitMsi <metroairplane@gmail.com>
2024-10-19 09:26:45 +0700

    update env

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+export POSTGRES_USERNAME=null
+export POSTGRES_PASSWORD=null
+export POSTGRES_HOST=null
+export POSTGRES_DB=null
+export AWS_BUCKET=null
+export AWS_REGION=null
+export AWS_PROFILE=null
+export JWT_SECRET=null
+export URL=null
