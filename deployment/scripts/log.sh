1573804220c0c80c55c4f13df4f6d55d1db78a62
ToanFitMsi <metroairplane@gmail.com>
2024-10-31 09:46:05 +0700

    update image

---
+++ deployment/scripts/log.sh
@@ -0,0 +1,7 @@
+# kubectl logs backend-feed-8dc48d44b-f69kg
+# kubectl logs backend-feed-8dc48d44b-k5wpn
+# kubectl logs backend-user-575c7b97c5-9tmxw
+# kubectl logs backend-user-575c7b97c5-khs2j
+# kubectl logs backend-user-575c7b97c5-qhgxl
+# kubectl logs frontend-7b99f67d9-zs7xj
+kubectl logs reverseproxy-856f8b66b4-tjd8c
